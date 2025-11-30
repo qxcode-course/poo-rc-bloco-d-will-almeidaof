67994678df02d736f9b030360210ef9233196f4d
will-almeidaof <willyamabr@gmail.com>
2025-11-30 12:12:14 +0000

    resolvendo a do contato

--- tasks/poo/contato/py/draft.py
+++ tasks/poo/contato/py/draft.py
@@ -30,3 +30,63 @@ class Contact:
         self.name = name
         self.favorited = False
         self.fone: list[Fone] = []
+
+
+    def addFone(self, id: str, number: str):
+        fone = Fone(id, number)
+        if fone.isValid():
+            self.fone.append(fone)
+        else:
+            print("fail: invalid number")
+
+    def rmFone(self, index: int):
+        if index <0 or index > len(self.fone):
+            print("fail: index errado")
+            return
+        self.fone.pop(index)
+
+
+    def offFavorite(self):
+        self.favorited = not self.favorited
+
+
+    def __str__(self):
+        lista = ", ".join([str(x) for x in self.fone])
+        if self.favorited:
+            return f"@ {self.name} [{lista}]"
+        else:
+            return f"- {self.name} [{lista}]"
+
+
+
+
+
+
+
+
+def main():
+    contact = Contact()
+    while True:
+        line = input()
+        print("$"+line)
+        args = line.split()
+        if args[0] == "end":
+            break
+        elif args[0] == "init":
+            name = args[1] if len(args) > 1 else ""
+            contact = Contact(name)
+        elif args[0] == "show":
+            print(contact)
+        elif args[0] == "add":
+            id = args[1]
+            number = args[2]
+            contact.addFone(id,number)
+        elif args[0] == "rm":
+            index = int(args[1])
+            contact.rmFone(index)
+        elif args[0] == "tfav":
+            contact.offFavorite()
+        else:
+            print("fail: comando invalido")
+
+main()
